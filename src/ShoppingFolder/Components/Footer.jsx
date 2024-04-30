0ac50626522da51ea7e7857c9e54c5bd4e5a7ab2
Sandeep Ganta <gantasandeep00@gmail.com>
2024-04-30 12:25:07 +0530

    React-shopping-UI

---
+++ src/ShoppingFolder/Components/Footer.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Footer=()=>
+{
+    return(
+        <div className="footerSection">
+           <h3>Footer</h3>
+           <img src="assets/footer/footer.png" alt="footer"/>
+        </div>
+    )
+}
+export default Footer;
---
+++ src/ShoppingFolder/data.js
@@ -0,0 +1,35 @@
+export const  Gents=
+{
+    title:"Gents Fashion",
+    image1:"assets/menImages/Men1.jpg",
+    image2:"assets/menImages/Men2.jpg",
+    image3:"assets/menImages/Men3.jpg",
+    image4:"assets/menImages/Men4.jpg",
+    image5:"assets/menImages/Men5.jpg",
+    image6:"assets/menImages/Men6.jpg",
+    price1:"459 INR",
+    price2:"559 INR",
+    price3:"659 INR",
+    price4:"759 INR",
+    price5:"859 INR",
+    price6:"959 INR",
+
+    
+}
+export const Ladies=
+{
+    title:"Ladies Fashion",
+    image1:"assets/womenImages/Women1.jpg",
+    image2:"assets/womenImages/Women2.jpg",
+    image3:"assets/womenImages/Women3.jpg",
+    image4:"assets/womenImages/Women4.jpg",
+    image5:"assets/womenImages/Women5.jpg",
+    image6:"assets/womenImages/Women6.jpg",
+
+    price1:"459 INR",
+    price2:"559 INR",
+    price3:"659 INR",
+    price4:"759 INR",
+    price5:"859 INR",
+    price6:"759 INR",
+}
